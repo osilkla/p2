c254ed721f4b2a843e31706116d70b8dd23f44dc
riol <riol@riols-mbp.home>
2023-06-30 14:55:14 +0200

    Add booklist from category url scraping function

--- app/index.py
+++ app/index.py
@@ -9,10 +9,21 @@ class Book:
         self.desc = desc
         self.price = price  
 
-
+categoryUrl = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 csvUrl = "books.csv"
-            
+ 
+def getBooksUrlFromCategoryUrl(url):
+    response = requests.get(url)
+    bookListUrl = []
+    if response.ok:  
+         for book in BeautifulSoup(response.text,"lxml").find_all('article'):
+            bookUrl = book.h3.a.get('href').replace('../../../', 'http://books.toscrape.com/catalogue/')
+            bookListUrl.append(bookUrl)
+            return bookListUrl       
+       
+getBooksUrlFromCategoryUrl(categoryUrl)
+
 def getBookFromUrl(url):
    response = requests.get(url)
    if response.ok:  
