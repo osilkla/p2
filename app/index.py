9b5700b32398627c4313d271c016403d7a7a739f
riol <riol@riols-mbp.home>
2023-06-30 13:48:07 +0200

    Add call to a book page and print infos

---
+++ app/index.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Book:
+    def __init__(self, title, author=None, desc=None, price=None):
+        self.title = title
+        self.author = author
+        self.desc = desc
+        self.price = price  
+
+
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+response = requests.get(url)
+if response.ok:  
+    soup = BeautifulSoup(response.text,"lxml")
+    title = soup.find('h1').text
+    price = soup.find('p', class_="price_color").text
+    book = Book(title, price=price)
+
+    
+    
+    print(book.__dict__)
