abb7595a6a50ceb67a23ea0d762288b38a5925c5
riol <riol@riols-mbp.home>
2023-07-03 13:30:25 +0200

    Add all books from one category in csv

--- app/index.py
+++ app/index.py
@@ -1,48 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+from BookClass import Book
+from Utils import initCSV,addBookToCSV
 
 
-class Book:
-    def __init__(self, title, author=None, desc=None, price=None):
-        self.title = title
-        self.author = author
-        self.desc = desc
-        self.price = price  
-
-categoryUrl = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
-url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-csvUrl = "books.csv"
+travelCategoryUrl = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+csvUrl = "travelBooks.csv"
  
-def getBooksUrlFromCategoryUrl(url):
-    response = requests.get(url)
-    bookListUrl = []
-    if response.ok:  
-         for book in BeautifulSoup(response.text,"lxml").find_all('article'):
-            bookUrl = book.h3.a.get('href').replace('../../../', 'http://books.toscrape.com/catalogue/')
-            bookListUrl.append(bookUrl)
-            return bookListUrl       
-       
-getBooksUrlFromCategoryUrl(categoryUrl)
+             
+travelBooksUrl = Book.getBooksUrlFromCategoryUrl(travelCategoryUrl)
 
-def getBookFromUrl(url):
-   response = requests.get(url)
-   if response.ok:  
-       return BeautifulSoup(response.text,"lxml")
-   
-def initCSV(csvUrl, header):
-     with open(csvUrl,"w") as outf:
-        outf.write(header)
+initCSV(csvUrl, 'title,price,url\n')
 
-def addBookToCSV(csvUrl, props):
-   with open(csvUrl,"a") as outf:
-        outf.write(props)
-        
-
-initCSV(csvUrl, 'title,price\n')
-
-soup = getBookFromUrl(url)
-title = soup.find('h1').text
-price = soup.find('p', class_="price_color").text
-book = Book(title, price=price)
-
-addBookToCSV(csvUrl,book.title+','+book.price+'\n')
+for bookUrl in travelBooksUrl:
+  soup = Book.getBookFromUrl(bookUrl)
+  title = soup.find('h1').text
+  price = soup.find('p', class_="price_color").text
+  book = Book(title, price=price, url=bookUrl)
+  addBookToCSV(csvUrl,book.title+','+book.price+book.url+'\n')
